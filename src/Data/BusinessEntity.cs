3e81dcc811549c97e8d57b58766eb0150213fa95
Michel Perez <michel.perez.saavedra@gmail.com>
2023-09-11 07:48:00 -0700

    feat: add the base BusinessEntity

---
+++ src/Data/BusinessEntity.cs
@@ -0,0 +1,178 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Oluso.Data;
+
+/// <summary>
+/// <inheritdoc cref="IBusinessEntity"/> it implements some basic
+/// equality comparators and other simple operators
+/// </summary>
+public class BusinessEntity<TKey, TUserKey> : IBusinessEntity<TKey, TUserKey>, IBusinessEntity, IEquatable<object>
+{
+    /// <summary>
+    /// returns a new <see cref="BusinessEntity{TKey,TUserKey}"/> instance
+    /// </summary>
+    public BusinessEntity()
+    {
+        CreateAt = DateTime.UtcNow;
+        RowVersion = Guid.NewGuid().ToString();
+    }
+
+    /// <summary>
+    /// returns a new <see cref="BusinessEntity{TKey,TUserKey}"/> instance
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="status"></param>
+    /// <param name="createAt"></param>
+    /// <param name="createdBy"></param>
+    /// <param name="updatedAt"></param>
+    /// <param name="updatedBy"></param>
+    /// <param name="deletedAt"></param>
+    /// <param name="deletedBy"></param>
+    /// <param name="rowVersion"></param>
+    public BusinessEntity(TKey id, bool status, DateTime createAt, TUserKey? createdBy, DateTime? updatedAt,
+        TUserKey? updatedBy, DateTime? deletedAt, TUserKey? deletedBy, string rowVersion)
+    {
+        Id = id;
+        Status = status;
+        CreateAt = createAt;
+        CreatedBy = createdBy;
+        UpdatedAt = updatedAt;
+        UpdatedBy = updatedBy;
+        DeletedAt = deletedAt;
+        DeletedBy = deletedBy;
+        RowVersion = rowVersion;
+    }
+
+    /// <summary>
+    /// returns a new <see cref="BusinessEntity{TKey,TUserKey}"/> instance
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="status"></param>
+    /// <param name="createdBy"></param>
+    public BusinessEntity(TKey id, bool status, TUserKey? createdBy)
+    {
+        Id = id;
+        Status = status;
+        CreatedBy = createdBy;
+        RowVersion = Guid.NewGuid().ToString();
+        CreateAt = DateTime.UtcNow;
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey, TUserKey}.Id"/>
+    /// </summary>
+    [Key]
+    [JsonPropertyName("id")]
+    public TKey Id { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.Status"/>
+    /// </summary>
+    [JsonPropertyName("status")]
+    public bool Status { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.CreateAt"/>
+    /// </summary>
+    [JsonPropertyName("createAt")]
+    public DateTime CreateAt { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.CreatedBy"/>
+    /// </summary>
+    [JsonPropertyName("createdBy")]
+    public TUserKey? CreatedBy { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.UpdatedAt"/>
+    /// </summary>
+    [JsonPropertyName("updateAt")]
+    public DateTime? UpdatedAt { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.UpdatedBy"/>
+    /// </summary>
+    [JsonPropertyName("updatedBy")]
+    public TUserKey? UpdatedBy { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey, TUserKey}.DeletedAt"/>
+    /// </summary>
+    [JsonPropertyName("deletedAt")]
+    public DateTime? DeletedAt { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.DeletedBy"/>
+    /// </summary>
+    [JsonPropertyName("deletedBy")]
+    public TUserKey? DeletedBy { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="IBusinessEntity{TKey,TUserKey}.RowVersion"/>
+    /// </summary>
+    [JsonPropertyName("rowVersion")]
+    public string RowVersion { get; set; }
+    #region Comparisons
+
+    /// <summary>
+    /// <inheritdoc cref="IEquatable{T}.Equals(T?)"/>
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object obj)
+    {
+        var compareTo = obj as BusinessEntity<TKey, TUserKey>;
+
+        if (ReferenceEquals(this, compareTo)) return true;
+        if (ReferenceEquals(null, compareTo)) return false;
+
+        return Id != null && Id.Equals(compareTo.Id);
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator ==( BusinessEntity<TKey, TUserKey> a,  BusinessEntity<TKey, TUserKey> b)
+    {
+        if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            return true;
+
+        if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            return false;
+
+        return a.Equals(b);
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator !=( BusinessEntity<TKey, TUserKey> a,  BusinessEntity<TKey, TUserKey> b)
+    {
+        return !Equals(a, b);
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="object.GetHashCode"/>
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="object.ToString"/>
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"{GetType().Name} [Id={Id}, RoVersion={RowVersion}]";
+    }
+
+    #endregion
+}
---
+++ src/Data/IBusinessEntity.cs
@@ -0,0 +1,65 @@
+namespace Oluso.Data;
+
+/// <summary>
+/// Base for all entities in the system
+/// </summary>
+public interface IBusinessEntity
+{
+}
+
+/// <summary>
+/// <inheritdoc cref="IBusinessEntity"/> and include some auditable members.
+/// This base class defines the Key and Creator user data types, to make
+/// more flexible the system modules
+/// </summary>
+/// <typeparam name="TKey"></typeparam>
+/// <typeparam name="TUserKey"></typeparam>
+public interface IBusinessEntity<TKey, TUserKey> : IBusinessEntity
+{
+    /// <summary>
+    /// Id if the entity
+    /// </summary>
+    TKey Id { get; set; }
+    
+    /// <summary>
+    /// status of the entity. this field is used to mark the entity when using soft deleted
+    /// mechanism, so if false system will not show it on any query result.
+    /// </summary>
+    bool Status { get; set; }
+    
+    /// <summary>
+    /// creation date
+    /// </summary>
+    DateTime CreateAt { get; set; }
+    
+    /// <summary>
+    /// how create the entity
+    /// </summary>
+    TUserKey? CreatedBy { get; set; }
+    
+    /// <summary>
+    /// update date
+    /// </summary>
+    DateTime? UpdatedAt { get; set; }
+    
+    /// <summary>
+    /// user id of user that update the entity
+    /// </summary>
+    TUserKey? UpdatedBy { get; set; }
+    
+    /// <summary>
+    /// deletion date. ths field is used only if a soft delete was applied of course 
+    /// </summary>
+    DateTime? DeletedAt { get; set; }
+    
+    
+    /// <summary>
+    /// deleted by, this field is used only if a soft deleted was applied of course
+    /// </summary>
+    TUserKey? DeletedBy { get; set; }
+    
+    /// <summary>
+    /// unique identifier to avoid entities conflicts 
+    /// </summary>
+    string RowVersion { get; set; }
+}
